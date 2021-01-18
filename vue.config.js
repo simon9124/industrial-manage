872491eb8a82f6fe3d6fca0df46af0cb4ea08bca
simon9124 <435776313@qq.com>
2021-01-18 20:34:38 +0800

    临时
    Signed-off-by: simon9124 <435776313@qq.com>

--- vue.config.js
+++ vue.config.js
@@ -39,8 +39,8 @@ module.exports = {
   devServer: {
     proxy: {
       "/network": {
-        // target: "http://172.15.35.194:9004/network",
-        target: "http://172.16.121.39:9004/network",
+        target: "http://172.15.35.194:9004/network",
+        // target: "http://172.16.121.39:9004/network",
         // target: "http://106.15.126.101:9004/network",
         changeOrigin: true,
         pathRewrite: {
