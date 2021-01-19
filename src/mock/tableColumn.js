10a8bdd1c6558b825734f80b088995a266b325ad
simon9124 <435776313@qq.com>
2021-01-19 17:53:12 +0800

    模拟数据更新
    Signed-off-by: simon9124 <435776313@qq.com>

--- src/mock/tableColumn.js
+++ src/mock/tableColumn.js
@@ -1,81 +1,81 @@
 // 表格列项 - 通道
 const passTagColumn = [
   {
-    prop: "name",
-    label: "名称（英文）",
-    minWidth: 120
+    prop: "source",
+    label: "名称（英文）"
+    // minWidth: 120
   },
   {
-    prop: "describe",
-    label: "描述（中文）",
-    minWidth: 160
+    prop: "description",
+    label: "描述（中文）"
+    // minWidth: 160
   },
   {
-    prop: "dataType",
-    label: "数据类型",
-    minWidth: 70
+    prop: "type",
+    label: "数据类型"
+    // minWidth: 70
   },
   {
-    prop: "direction",
-    label: "读写方向",
-    minWidth: 70
+    prop: "rw",
+    label: "读写方向"
+    // minWidth: 70
   },
   {
-    prop: "acquisitionCycle",
-    label: "采集周期",
-    minWidth: 70
-  },
-  {
-    prop: "IOTag",
-    label: "IO标签链接",
-    minWidth: 170
-  },
-  {
-    prop: "slaveStationID",
-    label: "从站ID",
-    minWidth: 55
-  },
-  {
-    prop: "registerType",
-    label: "寄存器类型",
-    minWidth: 85
-  },
-  {
-    prop: "registerAddr",
-    label: "寄存器地址",
-    minWidth: 85
-  },
-  {
-    prop: "dataFormat",
-    label: "数据格式",
-    minWidth: 70
+    prop: "cycle",
+    label: "采集周期"
+    // minWidth: 70
   }
+  // {
+  //   prop: "IOTag",
+  //   label: "IO标签链接",
+  //   minWidth: 170
+  // },
+  // {
+  //   prop: "slaveStationID",
+  //   label: "从站ID",
+  //   minWidth: 55
+  // },
+  // {
+  //   prop: "registerType",
+  //   label: "寄存器类型",
+  //   minWidth: 85
+  // },
+  // {
+  //   prop: "registerAddr",
+  //   label: "寄存器地址",
+  //   minWidth: 85
+  // },
+  // {
+  //   prop: "dataFormat",
+  //   label: "数据格式",
+  //   minWidth: 70
+  // }
 ];
 
 // 表格列项 - 设备
 const equipmentTagColumn = [
   {
-    prop: "name",
+    prop: "source",
     label: "名称（英文）",
     minWidth: 110
   },
   {
-    prop: "describe",
+    prop: "description",
     label: "描述（中文）",
     minWidth: 140
   },
   {
-    prop: "dataType",
+    prop: "type",
     label: "数据类型",
     minWidth: 70
   },
   {
-    prop: "direction",
+    prop: "rw",
     label: "读写方向",
     minWidth: 70
   },
   {
-    prop: "acquisitionCycle",
+    prop: "cycle",
     label: "采集周期",
     minWidth: 70
   },
@@ -118,11 +118,11 @@ const passTagHeader = {
   ],
   filterVal: [
     "index",
-    "name",
-    "describe",
-    "dataType",
-    "direction",
-    "acquisitionCycle",
+    "source",
+    "description",
+    "type",
+    "rw",
+    "cycle",
     "IOTag",
     "slaveStationID",
     "registerType",
@@ -148,11 +148,11 @@ const equipmentTagHeader = {
   ],
   filterVal: [
     "index",
-    "name",
-    "describe",
-    "dataType",
-    "direction",
-    "acquisitionCycle",
+    "source",
+    "description",
+    "type",
+    "rw",
+    "cycle",
     "registerType",
     "registerAddr",
     "analyticalMethod",
@@ -164,11 +164,11 @@ const equipmentTagHeader = {
 // 表头中英文对照
 const tagTranslation = {
   index: "序号",
-  name: "名称（英文）",
-  describe: "描述（中文）",
-  dataType: "数据类型",
-  direction: "读写方向",
-  acquisitionCycle: "采集周期",
+  source: "名称（英文）",
+  description: "描述（中文）",
+  type: "数据类型",
+  rw: "读写方向",
+  cycle: "采集周期",
   IOTag: "IO标签链接",
   slaveStationID: "从站ID",
   registerType: "寄存器类型",
